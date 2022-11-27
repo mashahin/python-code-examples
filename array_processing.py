0199ccd72fc4a21c0326900ecb29cda6610d88ac
Muhammad A Shahin <mashahin2009@gmail.com>
2022-11-27 11:17:13 -0600

    Initial commit

--- array_processing.py
+++ array_processing.py
@@ -22,11 +22,12 @@ def array_product(input_arr):
 def array_sum_of_products(input_arr):
     """
     compute sum of products of array element pairs
+    input array of n integer elements reduces to n-1 elements 
 
     """
     n = len(input_arr)
-    out_arr = np.array(zeros(n), dtype=int)
-    for i in range(n):
+    out_arr = np.array(zeros(n-1), dtype=int)
+    for i in range(n-1):
         sum = 0
         for j in range(i+1, n):
             sum += input_arr[i]*input_arr[j]
@@ -34,9 +35,10 @@ def array_sum_of_products(input_arr):
     return out_arr
 
 
+# array elements can take on zero, +ve, or -ve integer values
 # a = np.array([0, 1, 2, 3, 4], dtype=int)
 # a = np.array([], dtype=int)
-a = np.array([1, -2, -3, 4], dtype=int)
+a = np.array([1, -2, 3, 4], dtype=int)
 print(f"input array: {a}")
 print(f"products array: {array_product(a)}")
 print(f"sum of products: {array_sum_of_products(a)}")
