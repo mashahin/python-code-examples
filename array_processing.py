487e80c3bb8a79d09cc66ea22c7199f75e558d21
Muhammad A Shahin <mashahin2009@gmail.com>
2022-11-26 19:05:55 -0600

    Initial commit

---
+++ array_processing.py
@@ -0,0 +1,43 @@
+# array functions
+import numpy as np
+from numpy import zeros
+
+
+def array_product(input_arr):
+    """
+    compute product of array elements except self
+
+    """
+    n = len(input_arr)
+    out_arr = np.array(zeros(n, dtype=int))
+    for i in range(n):
+        product = 1
+        for j in range(n):
+            if (i != j):
+                product *= input_arr[j]
+        out_arr[i] = product
+    return out_arr
+
+
+def array_sum_of_products(input_arr):
+    """
+    compute sum of products of array element pairs
+
+    """
+    n = len(input_arr)
+    out_arr = np.array(zeros(n), dtype=int)
+    for i in range(n):
+        sum = 0
+        for j in range(i+1, n):
+            sum += input_arr[i]*input_arr[j]
+        out_arr[i] = sum
+    return out_arr
+
+
+# a = np.array([0, 1, 2, 3, 4], dtype=int)
+# a = np.array([], dtype=int)
+a = np.array([1, -2, -3, 4], dtype=int)
+print(f"input array: {a}")
+print(f"products array: {array_product(a)}")
+print(f"sum of products: {array_sum_of_products(a)}")
+print(f"array inverted: {a[::-1]}")
