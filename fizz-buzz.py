07d54815e88eb8954b74f17adb98abe6b663fa67
Muhammad A Shahin <mashahin2009@gmail.com>
2022-12-28 17:03:54 -0600

    Initial commit

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -3,7 +3,9 @@
 # answer[i] == "Fizz" if i is divisible by 3.
 # answer[i] == "Buzz" if i is divisible by 5.
 # answer[i] == i(as a string) if none of the above conditions are true.
-def fizzbuzz(n):
+
+# A simple iterative version
+def fizzBuzzSimple(n):
     answer = []
     for i in range(1, n+1):
         if (i % 3 == 0 and i % 5 == 0):
@@ -17,5 +19,11 @@ def fizzbuzz(n):
     return answer
 
 
-n = 30
-print(fizzbuzz(n))
+# A cleaner version
+def fizzBuzzClean(n):
+    return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+
+
+n = 15
+print(fizzBuzzSimple(n))
+print(fizzBuzzClean(n))
