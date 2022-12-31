868d0e9cb0b47e3821345e293d6a0921897e7bd8
Muhammad A Shahin <mashahin2009@gmail.com>
2022-12-31 09:01:19 -0600

    Initial commit

--- python-data-classes.py
+++ python-data-classes.py
@@ -1,4 +1,7 @@
 # Python class
+from dataclasses import dataclass, field
+
+
 class Investor1:
 
     def __init__(self, name: str, age: int, cash: float) -> None:
@@ -6,9 +9,23 @@ class Investor1:
         self.age = age
         self.cash = cash
 
+    # We need to manually add desired functionality
+
     def __repr__(self):
         return f"name: {self.name}, age: {self.age}"
 
+    def __eq__(self, other) -> bool:
+        return self.name == other.name
 
-i1 = Investor1("John", 30, 12000)
-print(i1)  # It prints the object <__main__.Investor1 object at 0x0000018A1E675DF0>
+
+i1 = Investor1("John", 25, 10000)
+i2 = Investor1("Anna", 30, 15000)
+i3 = Investor1("Bob", 70, 800000)
+i4 = Investor1("John", 25, 10000)  # same as i1
+print(i1)
+print(i1 == i4)
+
+
+# Python Data classes let you access inbuilt functionality
+# @ dataclass
+# class Investor2:
