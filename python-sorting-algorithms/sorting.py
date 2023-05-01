e8e0235062ed0525347be005611f79960c4215fb
mashahin <mashahin2009@gmail.com>
2023-05-01 00:10:15 -0500

    Add merge sot

--- python-sorting-algorithms/sorting.py
+++ python-sorting-algorithms/sorting.py
@@ -3,6 +3,7 @@ from timeit import repeat
 from bubble_sort import bubblesort
 from quick_sort import quicksort
 from insertion_sort import insertion_sort
+from merge_sort import *
 
 def run_sorting_algorithm(algorithm, array):
     # Set up the context and prepare the call to the specified
@@ -36,3 +37,4 @@ if __name__ == "__main__":
     run_sorting_algorithm(algorithm="bubblesort", array=array)
     run_sorting_algorithm(algorithm="quicksort", array=array)
     run_sorting_algorithm(algorithm="insertion_sort", array=array)
+    run_sorting_algorithm(algorithm="merge_sort", array=array)
